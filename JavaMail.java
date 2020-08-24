a9c06bcb0786d81ad8a94791900329a7c3ac25f8
kk289 <kevilkhadka@gmail.com>
2020-08-24 16:02:09 -0700

    main java file

---
+++ JavaMail.java
@@ -0,0 +1,7 @@
+package sendMail;
+
+public class JavaMail {
+    public static void main(String[] args) throws Exception{
+    	JavaMailTest.sendMail("example@gmail.com"); // enter gmail: receiver
+    }
+}
